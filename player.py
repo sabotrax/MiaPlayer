fc50ba1a3108e7df19ac743c77467ab17683e1eb
Marcus Schommer <marcus@dankesuper.de>
2021-12-15 20:24:38 +0100

    Roemische Zahlen funktionieren im Prinzip.

--- player.py
+++ player.py
@@ -96,20 +96,21 @@ def kitt():
     # darstellen
 
 def into_roman(number):
-    num = [1, 4, 5, 9, 10, 40, 50, 90,
-        100, 400, 500, 900, 1000]
-    sym = ["I", "IV", "V", "IX", "X", "XL",
-        "L", "XC", "C", "CD", "D", "CM", "M"]
-    i = 12
+    # non-subtraction notation
+    num = [1, 5, 10, 50, 100, 500, 1000]
+    sym = ["I", "V", "X", "L", "C", "D", "M"]
+    i = 6
 
+    roman_number = ""
     while number:
         div = number // num[i]
         number %= num[i]
 
         while div:
-            print(sym[i], end = "")
+            roman_number = roman_number + sym[i]
             div -= 1
         i -= 1
+    return roman_number
 
 def main():
     reader = SimpleMFRC522()
--- roman.py
+++ roman.py
@@ -4,32 +4,26 @@
 def into_roman(number):
     #num = [1, 4, 5, 9, 10, 40, 50, 90,
         #100, 400, 500, 900, 1000]
+    # non-subtraction notation
     num = [1, 5, 10, 50, 100, 500, 1000]
     #sym = ["I", "IV", "V", "IX", "X", "XL",
         #"L", "XC", "C", "CD", "D", "CM", "M"]
     sym = ["I", "V", "X", "L", "C", "D", "M"]
     i = 6
 
+    roman_number = ""
     while number:
         div = number // num[i]
         number %= num[i]
 
         while div:
-            print(sym[i], end = "")
+            #print(sym[i], end = "")
+            roman_number = roman_number + sym[i]
             div -= 1
         i -= 1
-    print()
+    #print()
+    return roman_number
 
-into_roman(1)
-into_roman(4)
-into_roman(5)
-into_roman(5)
-into_roman(6)
-into_roman(9)
-into_roman(10)
-into_roman(13)
-into_roman(14)
-into_roman(16)
-into_roman(19)
-into_roman(22)
-into_roman(1023)
+for i in range(1, 50):
+    rn = into_roman(i)
+    print(rn + " " + str(len(rn)))
